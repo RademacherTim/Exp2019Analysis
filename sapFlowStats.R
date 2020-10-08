6be1383add9eea6006f2fcee770bc29d70a05bde
TTRademacher <rademacher.tim@gmail.com>
2020-10-07 22:16:32 -0400

    Added some stats

--- sapFlowStats.R
+++ sapFlowStats.R
@@ -42,6 +42,38 @@ sapFlowData <- sapFlowData %>% mutate (period, periodAlt)
 
 # calculate mean sap flow velocity and standard deviation by group during the experiment
 #----------------------------------------------------------------------------------------
-sapFlowData %>% group_by (treatment) %>% filter ()
+sapFlowData %>% group_by (treatment, period) %>% 
+  filter (datetime < as_datetime ('2019-11-01')) %>%
+  summarise (mean.v.sap.1 = mean (v.sap.1, na.rm = TRUE),
+             se.v.sap.1   = se   (v.sap.1),
+             mean.v.sap.2 = mean (v.sap.2, na.rm = TRUE),
+             se.v.sap.2   = se   (v.sap.2),
+             mean.v.sap.3 = mean (v.sap.3, na.rm = TRUE),
+             se.v.sap.3   = se   (v.sap.3))
 
+# wrangle data into long format
+#----------------------------------------------------------------------------------------
+temp <- pivot_longer (data = sapFlowData, cols = c (v.sap.1, v.sap.2, v.sap.3), 
+                      names_to = 'depth', names_prefix = 'v.sap.', 
+                      values_to = 'v.sap')
+
+# convert variable to factors
+#----------------------------------------------------------------------------------------
+temp <- temp %>%
+  mutate (datetime  = factor (datetime),
+          tree      = factor (tree),
+          treatment = factor (treatment, levels = c (5, 1)),
+          period    = factor (period,    levels = c ('during','after','before')),
+          periodAlt = factor (periodAlt, levels = c ('chilling','non-chilling')),
+          depth     = factor (depth,     levels = 3:1))
+
+# fit mixed effects model to wood sugar concentrations with tree and height as random 
+# effects to account for idiosyncratic differences due to factors such as variations 
+# in exact azimuth or systematic difference between trees
+#----------------------------------------------------------------------------------------
+M1 <- lmer (formula = v.sap ~ (1 | tree) + depth + datetime + period:treatment, 
+            data = temp,
+            REML = TRUE)
+summary (M1)
+# TR Chweck whether there is another Inf in one of the v.sap, because the fixed effect model crashed.
 #========================================================================================
