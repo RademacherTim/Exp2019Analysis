1841a9509a115c97a1f7c3a13b2e8d81043315f4
TTRademacher <rademacher.tim@gmail.com>
2021-06-16 21:58:18 -0400

    Started looking at potential differences in cell-wall area.

--- analyseCellWallArea.R
+++ analyseCellWallArea.R
@@ -56,6 +56,19 @@ ROIbounds <- read_csv (paste0 (iDir,'Exp2019BoundariesForRegionOfInterest.csv'),
 #----------------------------------------------------------------------------------------
 data <- files %>% dplyr::left_join (ROIbounds, by = 'image.name'); rm (ROIbounds)
 
+# add the area of the 2018 and 2019 region of interest (micrometer^2)
+#----------------------------------------------------------------------------------------
+res <- 1.5 # resolution of image scans (pixels per micrometer) 
+data <- data %>% 
+  mutate (A2018ROI = ((xMax2018 - xMin2018) * (yMax2018 - yMin2018)) / res,
+          A2019ROI = ((xMax2019 - xMin2019) * (yMax2019 - yMin2019)) / res) 
+
+# add column with percentage CWA for the 2018 and 2019 ring region of interest 
+#----------------------------------------------------------------------------------------
+data <- add_column (data, 
+                    perCWA2018 = NA, 
+                    perCWA2019 = NA)
+
 # loop over image names
 #----------------------------------------------------------------------------------------
 for (i in 1:dim (data) [1]) {
@@ -69,43 +82,84 @@ for (i in 1:dim (data) [1]) {
   img <- imager::load.image (paste0 (iDir,'images/Exp2019/JPG/',iName))
   #img <- magick::image_read (path = paste0 (iDir,'images/Exp2019/JPG/',iName))
   # The lower the value the whiter the image in grayscale
-  
+
+  # Whether to plot the image with the regions of interest
+  #--------------------------------------------------------------------------------------
+  PLOT <- FALSE
+
   # plot image with cropped area highlighted
   #--------------------------------------------------------------------------------------
-  plot (img)
-  rect (xleft   = files [['xMin2018']] [i],
-        xright  = files [['xMax2018']] [i], 
-        ybottom = files [['yMin2018']] [i],
-        ytop    = files [['yMax2018']] [i],
-        border  = tColours [['colour']] [3], lwd = 2)
-  rect (xleft   = files [['xMin2019']] [i],
-        xright  = files [['xMax2019']] [i], 
-        ybottom = files [['yMin2019']] [i],
-        ytop    = files [['yMax2019']] [i],
-        border  = tColours [['colour']] [6], lwd = 2)
-  
-  # crop image to region of interest for 2018 and 2019 ring
+  if (PLOT) {
+    plot (img)
+    rect (xleft   = files [['xMin2018']] [i],
+          xright  = files [['xMax2018']] [i],
+          ybottom = files [['yMin2018']] [i],
+          ytop    = files [['yMax2018']] [i],
+          border  = tColours [['colour']] [3], lwd = 2)
+    rect (xleft   = files [['xMin2019']] [i],
+          xright  = files [['xMax2019']] [i],
+          ybottom = files [['yMin2019']] [i],
+          ytop    = files [['yMax2019']] [i],
+          border  = tColours [['colour']] [6], lwd = 2)
+  }
+
+  # Whether to plot the regions of interest
   #--------------------------------------------------------------------------------------
-  imgROI2018 <- imager::imrotate (img, angle = data [['img.rotation']] [i]) %>%
-    imager::imsub (x < data [['xMax2018']] [i],
-                   x > data [['xMin2018']] [i],
-                   y < data [['yMax2018']] [i],
-                   y > data [['yMin2018']] [i])
-  imgROI2018.g <- grayscale (imgROI2018)# %>% isoblur (10)
-  plot (imgROI2018.g)
-  imgROI2019 <- imager::imrotate (img, angle = data [['img.rotation']] [i]) %>%
-    imager::imsub (x < data [['xMax2019']] [i],
-                   x > data [['xMin2019']] [i],
-                   y < data [['yMax2019']] [i], 
-                   y > data [['yMin2019']] [i])
-  imgROI2019.g <- grayscale (imgROI2019) #%>% isoblur (10)
-  plot (imgROI2019.g)
+  PLOT <- FALSE
+
+  # crop image to region of interest for 2018 ring
+  #--------------------------------------------------------------------------------------
+  if (!is.na (data [['xMax2018']] [i])) {
+    imgROI2018 <- imager::imrotate (img, angle = data [['img.rotation']] [i]) %>%
+      imager::imsub (x < data [['xMax2018']] [i],
+                     x > data [['xMin2018']] [i],
+                     y < data [['yMax2018']] [i],
+                     y > data [['yMin2018']] [i])
+    imgROI2018.g <- grayscale (imgROI2018) 
+    if (PLOT) plot (imgROI2018.g)
+  } else {
+    imgROI2018.g <- NA
+  }
+
+  # crop image to region of interest for 2019 ring
+  #--------------------------------------------------------------------------------------
+  if (!is.na (data [['xMax2019']] [i])) {
+    imgROI2019 <- imager::imrotate (img, angle = data [['img.rotation']] [i]) %>%
+      imager::imsub (x < data [['xMax2019']] [i],
+                     x > data [['xMin2019']] [i],
+                     y < data [['yMax2019']] [i], 
+                     y > data [['yMin2019']] [i])
+    imgROI2019.g <- grayscale (imgROI2019)
+    if (PLOT) plot (imgROI2019.g)
+  } else {
+    imgROI2019.g <- NA
+  }
   
-  #imgCrop.g <- grayscale (imgCrop) %>% isoblur (10)
-  #plot (imgCrop.g)
-  #thrImg <- imgCrop.g %>% threshold (thr = 0.8) %>% plot () # TR I could go several thresholds and choose the one before there is a big jump in "cell-wal area", which would correspond to when vessels are included 
-  #perCWA <- sum (thrImg) / (dim (thrImg) [1] * dim (thrImg) [2]) * 100
+  # estimate percentage of cell-wall area in image using simple threshold
+  #--------------------------------------------------------------------------------------
+  threshold <- 0.7
+  # TR I could go several thresholds and choose the one before there is a big jump in "cell-wal area", which would correspond to when vessels are included
   
-  # Need to label the vessels to count how many there are in the image
+  if (!is.na (imgROI2018.g [1])) {
+    thrImg2018 <- imgROI2018.g %>% threshold (thr = threshold) #%>% 
+      #plot (main = paste (i, data [['tree.id']] [i], data [['sample.height']] [i]))  
+    data [['perCWA2018']] [i] <- sum (thrImg2018) / (dim (thrImg2018) [1] * dim (thrImg2018) [2]) * 100
+  }
+  if (!is.na (imgROI2019.g [1])) {
+    thrImg2019 <- imgROI2019.g %>% threshold (thr = threshold) #%>% 
+      #plot (main = paste (i, data [['tree.id']] [i], data [['sample.height']] [i]))
+    data [['perCWA2019']] [i] <- sum (thrImg2019) / (dim (thrImg2019) [1] * dim (thrImg2019) [2]) * 100
+  }
+  
+  #--------------------------------------------------------------------------------------
+  # De-noising with isoblur reduces the amount of cell-wall area substantially, but 
+  # results remain highly correlated with the same threshold, suggesting that they are 
+  # robust.
+  # Choosing a different threshold of 0.7 reduces the correlation slightly, but it still
+  # the overall results still hold up robustly.
+  
+  
+  # Need to label the vessels to count how many there are in each image
+  #--------------------------------------------------------------------------------------
 } # end file loop 
 #========================================================================================
